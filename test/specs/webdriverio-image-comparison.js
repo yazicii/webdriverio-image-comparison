96c7bc2f35662bceeee49b055f721c015b780625
oguzhan.yazici <oguzhan.yazici@enuygun.com>
2024-01-09 09:26:39 +0300

    second

--- test/specs/webdriverio-image-comparison.js
+++ test/specs/webdriverio-image-comparison.js
@@ -2,17 +2,46 @@
 
 describe('gorsel karsilastirma', () => {
     beforeEach(async () => {
-        await browser.url('https://www.enuygun.com/kredi/');
+        await browser.url('https://www.enuygun.com/mevduat/');
         await browser.maximizeWindow();
 
     });
 
-    it('Elementin Görsel Karşılaştırması', async () => {
-        const element = await $('[data-testid="landingHero-ihtiyac__find-loan__button"]');
+    // it('button', async () => {
+    //     const element = await $('[data-testid="landingHeadLine-find-deposit-find-deposit-interest__button"]');
+    //     await element.waitForDisplayed({ timeout: 10000 });
+
+
+    //     expect(await browser.checkElement(element, 'current',{})).toEqual(95);
+
+    // });
+
+    // it('input alani', async () => {
+    //     const element = await $('[class="DepositInterestHeroInputs_MultipleInputDiv__HMqBW"]');
+    //     await element.waitForDisplayed({ timeout: 10000 });
+
+
+    //     expect(await browser.checkElement(element, 'input',{})).toEqual(95);
+
+    // });
+
+    // it('tablo', async () => {
+    //     const element = await $('[data-testid="landingPopularDepositCalculatorTable__table__carousel"]');
+    //     await element.waitForDisplayed({ timeout: 10000 });
+
+
+    //     expect(await browser.checkElement(element, 'tablo',{})).toEqual(95);
+
+    // });
+
+    it('oguz', async () => {
+        const element = await $('[class="DepositInterestHeroInputs_MultipleInputDiv__HMqBW"]');
         await element.waitForDisplayed({ timeout: 10000 });
 
 
-        expect(await browser.checkElement(await $('[data-testid="landingHero-ihtiyac__find-loan__button"]'), 'current',{})).toEqual(0);
+        expect(await browser.checkElement(element, 'tablo',{})).toEqual(50);
 
     });
+
+
 });
